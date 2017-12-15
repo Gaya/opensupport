8e30ab235d84ea5f1c2455c20ad3435f412ab53a
Gaya Kessler <gaya.kessler@gmail.com>
2017-12-15 15:51:55 +0100

    create webserver

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
+import Koa from 'koa';
+import multer from 'koa-multer';
+import fs from 'fs';
+
 import { maintainersCountOfProject } from './maintainers';
 
-import packageJson from './example-package';
+const app = new Koa();
+const upload = multer({ dest: 'tmp/' });
 
-const dependencies = Array.from(new Set([
-  ...Object.keys(packageJson.dependencies),
-  ...Object.keys(packageJson.dependencies),
-]));
+function jsonToDependencies(info) {
+  return Array.from(new Set([
+    ...Object.keys(info.dependencies),
+    ...Object.keys(info.devDependencies),
+  ]));
+}
 
-maintainersCountOfProject(dependencies)
-  .then(maintainers => {
-    console.log(maintainers);
+function readUpload(path) {
+  return new Promise(resolve => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      fs.unlink(path, () => {});
+
+      resolve(data.toString());
+    });
   });
+}
+
+app.use(upload.single('packageJson'));
+
+app.use(async (ctx) => {
+  try {
+    const data = await readUpload(ctx.req.file.path);
+
+    const packageJson = JSON.parse(data.toString());
+    const dependencies = jsonToDependencies(packageJson);
+
+    const maintainers = await maintainersCountOfProject(dependencies);
+    ctx.body = JSON.stringify(maintainers);
+  } catch (e) {
+    ctx.throw(500, e.message);
+  }
+});
+
+app.listen(process.env.PORT || 1337);
+console.log('Listening to 1337');
