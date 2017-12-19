f2f7c50764ed4bbd240729d751d270d330163327
Gaya Kessler <gaya.kessler@gmail.com>
2017-12-19 07:44:28 +0100

    read json

--- index.js
+++ index.js
@@ -32,17 +32,16 @@ function readUpload(path) {
 const router = new Router();
 
 router.post('/scan', upload.single('packageJson'), async (ctx) => {
-  ctx.body = JSON.stringify(ctx.req.file.path);
-  return;
-
   try {
     const data = await readUpload(ctx.req.file.path);
 
     const packageJson = JSON.parse(data.toString());
     const dependencies = jsonToDependencies(packageJson);
 
-    const maintainers = await maintainersCountOfProject(dependencies);
-    ctx.body = JSON.stringify(maintainers);
+    ctx.body = JSON.stringify(dependencies);
+
+    // const maintainers = await maintainersCountOfProject(dependencies);
+    // ctx.body = JSON.stringify(maintainers);
   } catch (e) {
     ctx.throw(500, e.message);
   }
