569fc5e70026a41fcf6f0955580168c79a8d6c92
Gaya Kessler <gaya.kessler@gmail.com>
2017-12-21 11:04:01 +0100

    add extract text plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './app/index',
@@ -15,20 +16,15 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
-
       options: {
         presets: ['es2015'],
       },
     }, {
       test: /\.(scss|css)$/,
-
-      use: [{
-        loader: 'style-loader',
-      }, {
-        loader: 'css-loader',
-      }, {
-        loader: 'sass-loader',
-      }],
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader'],
+      }),
     }],
   },
 
@@ -38,8 +34,14 @@ module.exports = {
       template: 'app/index.html',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
       API_URL: JSON.stringify(process.env.API_URL || 'http://localhost:3030'),
+    }),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      disable: process.env.NODE_ENV !== 'production',
     }),
   ],
 };
