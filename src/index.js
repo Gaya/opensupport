9051df678e2b515ff367c68f3ee83ba731702dc8
Gaya Kessler <gaya.kessler@gmail.com>
2017-12-28 10:54:45 +0100

    history api fallback

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import Koa from 'koa';
 import koaStatic from 'koa-static';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
+import historyFallback from 'koa2-history-api-fallback';
 
 import scan from './scan';
 
 const app = new Koa();
 
+app.use(historyFallback());
 app.use(koaStatic(`${__dirname}/../public`));
 app.use(cors());
 app.use(bodyParser());
