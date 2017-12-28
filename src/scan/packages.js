6a2420cd10e1e54ace782ca8ee83e0c6eacc68bc
Gaya Kessler <gaya.kessler@gmail.com>
2017-12-28 16:44:06 +0100

    return maintainers with packages

--- src/scan/packages.js
+++ src/scan/packages.js
@@ -1,8 +1,26 @@
 import { sortByCount } from './helpers';
 
 function sumProjects(totals, lib) {
-  const totalsCopy = { ...totals };
-  totalsCopy[lib.name] = (totalsCopy[lib.name] || 0) + 1;
+  let totalsCopy = [...totals];
+
+  if (totals.find(item => item.name === lib.name)) {
+    totalsCopy = totalsCopy.map((item) => {
+      if (item.name === lib.name) {
+        return {
+          ...item,
+          count: item.count + 1,
+        };
+      }
+
+      return item;
+    });
+  } else {
+    totalsCopy = [...totalsCopy, {
+      name: lib.name,
+      count: 1,
+      maintainers: lib.maintainers.map(maintainer => maintainer.name),
+    }];
+  }
 
   if (lib.children) {
     return lib.children.reduce((childTotals, child) => sumProjects(childTotals, child), totalsCopy);
@@ -12,11 +30,9 @@ function sumProjects(totals, lib) {
 }
 
 export function packageCountOfProject(info) {
-  const libs = info.reduce(sumProjects, {});
+  const libs = info.reduce(sumProjects, []);
 
-  const libsSorted =
-    [...Object.keys(libs).map(key => ({ name: key, count: libs[key] }))]
-      .sort(sortByCount);
+  const libsSorted = [...libs].sort(sortByCount);
 
   return libsSorted.filter((item, index) => index < 20);
 }
